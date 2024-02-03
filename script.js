7bd9e98cdf7aeafab3b6019d462f4ea7bb43dba9
Adeebkhan Pathan <akpathan2799@gmail.com>
2024-02-03 16:25:22 +0530

    commit

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function startNewGameFunctionality() {
     }
 
     
-    gamePlayMusic.volume=0.3;
+    gamePlayMusic.volume=0.2;
     gamePlayMusic.play();
     score.innerText = '0';
     timer.innerText = '60'
